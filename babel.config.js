d97400e584f25093f443787116155a812b8b914c
yuanxiaoran <hmxs_hmbb@163.com>
2024-12-11 14:30:54 +0800

    知识社区 WebApp 项目初始化已完成

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    ['@vue/cli-plugin-babel/preset', { modules: false }]
+    // ['es2015', { modules: false }]
+  ],
+  plugins: [
+    [
+      'component',
+      {
+        libraryName: 'mint-ui',
+        style: true
+      }
+    ]
   ]
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+import { Button, Cell } from 'mint-ui'
 
 Vue.use(VueRouter)
+Vue.component(Button.name, Button)
+Vue.component(Cell.name, Cell)
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+const routes = []
 
 const router = new VueRouter({
   routes
