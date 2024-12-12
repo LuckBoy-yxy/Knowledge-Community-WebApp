3aed53207139f7cc9b2b523145b08688f6f6f687
yuanxiaoran <hmxs_hmbb@163.com>
2024-12-12 13:24:18 +0800

    svgIcon 公共组件已封装完成

---
+++ src/assets/icons/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import SvgIcon from '@/components/SvgIcon.vue'
+
+Vue.component('svg-icon', SvgIcon)
+
+const icons = require.context('./svg', false, /\.svg$/)
+// console.log(icons)
+// console.log(icons.keys())
+const requireAll = requireContext => requireContext.keys().map(requireContext)
+
+// console.log(requireAll(icons))
+requireAll(icons)
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,28 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
+const path = require('path')
+
+const resolve = (dir) => path.join(__dirname, dir)
+
+module.exports = {
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule('svg')
+    svgRule.uses.clear()
+    svgRule
+      .test(/\.svg$/)
+      .include.add(resolve('./src/assets/icons'))
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]'
+      })
+    config.module
+      .rule('images')
+      .test(/\.(png|jpe?g|gif|webp|svg)(\?.*)?$/)
+      .exclude.add(resolve('./src/assets/icons'))
+  }
+}
